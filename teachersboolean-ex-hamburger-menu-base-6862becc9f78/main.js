d9a14eeaf18ca92728059670d76f0a2ac8ff8c4b
Maaax02 <massiown@gmail.com>
2021-12-09 16:58:54 +0100

    esercizio finito

--- teachersboolean-ex-hamburger-menu-base-6862becc9f78/main.js
+++ teachersboolean-ex-hamburger-menu-base-6862becc9f78/main.js
@@ -1,21 +1,13 @@
-// let unknown = document.querySelector('.close');
-// let hamburger = document.querySelector('.hamburger-menu');
-
-// unknown = addEventListener('click', function(){
-//     document.getElementsByClassName('hamburger-menu').styleas
-    
-// })
-
 
 let toggler = document.getElementById('toggler');
 let unknown = document.getElementById('close');
 let hamburger = document.getElementById('hamburger')
 
 
-toggler = addEventListener('click', function(){
-    document.getElementById('hamburger').style.display='block';
+toggler.addEventListener('click', function(){
+    hamburger.style.display='block';
 })
 
-unknown = addEventListener('click', function(){
-    document.getElementById('hamburger').style.display='none';
+unknown.addEventListener('click', function(){
+    hamburger.style.display='none';
 })
